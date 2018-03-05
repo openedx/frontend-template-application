da4d3be918c59c250f56ad842c7802c0366f3894
Tyler Hallada <thallada@edx.org>
2018-03-05 15:14:26 -0500

    fix(webpack): remove css modules and add comments

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -1,3 +1,5 @@
+// This is the dev Webpack config. All settings here should prefer a fast build
+// time at the expense of creating larger, unoptimized bundles.
 const Merge = require('webpack-merge');
 const path = require('path');
 const webpack = require('webpack');
@@ -13,7 +15,10 @@ module.exports = Merge.smart(commonConfig, {
     path.resolve(__dirname, '../src/index.jsx'),
   ],
   module: {
+    // Specify file-by-file rules to Webpack. Some file-types need a particular kind of loader.
     rules: [
+      // The babel-loader transforms newer ES2015+ syntax to older ES5 for older browsers.
+      // Babel is configured with the .babelrc file at the root of the project.
       {
         test: /\.(js|jsx)$/,
         include: [
@@ -21,23 +26,26 @@ module.exports = Merge.smart(commonConfig, {
         ],
         loader: 'babel-loader',
         options: {
+          // Caches result of loader to the filesystem. Future builds will attempt to read from the
+          // cache to avoid needing to run the expensive recompilation process on each run.
           cacheDirectory: true,
         },
       },
+      // We are not extracting CSS from the javascript bundles in development because extracting
+      // prevents hot-reloading from working, it increases build time, and we don't care about
+      // flash-of-unstyled-content issues in development.
       {
         test: /(.scss|.css)$/,
         use: [
-          'style-loader',
+          'style-loader', // creates style nodes from JS strings
           {
-            loader: 'css-loader',
+            loader: 'css-loader', // translates CSS into CommonJS
             options: {
               sourceMap: true,
-              modules: true,
-              localIdentName: '[local]',
             },
           },
           {
-            loader: 'sass-loader',
+            loader: 'sass-loader', // compiles Sass to CSS
             options: {
               sourceMap: true,
               includePaths: [
@@ -48,19 +56,27 @@ module.exports = Merge.smart(commonConfig, {
           },
         ],
       },
+      // Webpack, by default, uses the url-loader for images and fonts that are required/included by
+      // files it processes, which just base64 encodes them and inlines them in the javascript
+      // bundles. This makes the javascript bundles ginormous and defeats caching so we will use the
+      // file-loader instead to copy the files directly to the output directory.
       {
         test: /\.(woff2?|ttf|svg|eot)(\?v=\d+\.\d+\.\d+)?$/,
         loader: 'file-loader',
       },
     ],
   },
+  // Specify additional processing or side-effects done on the Webpack output bundles as a whole.
   plugins: [
+    // Generates an HTML file in the output directory.
     new HtmlWebpackPlugin({
-      inject: true,
+      inject: true, // Appends script tags linking to the webpack bundles at the end of the body
       template: path.resolve(__dirname, '../public/index.html'),
     }),
     new webpack.HotModuleReplacementPlugin(),
   ],
+  // This configures webpack-dev-server which serves bundles from memory and provides live
+  // reloading.
   devServer: {
     host: '0.0.0.0',
     port: 1991,
