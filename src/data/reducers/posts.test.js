524954096245748bb5030bb7a57c0f9c23883088
jaebradley <jae.b.bradley@gmail.com>
2018-03-19 16:37:26 -0400

    tests(post-reducer): add unit test for post reducer

---
+++ src/data/reducers/posts.test.js
@@ -0,0 +1,41 @@
+import posts from './posts';
+import { GET_POSTS, STARTED_FETCHING_POSTS, FINISHED_FETCHING_POSTS } from '../constants/ActionType';
+
+const initialState = {
+  posts: [],
+  startedFetching: false,
+  finishedFetching: false,
+};
+
+describe('posts reducer', () => {
+  it('has initial state', () => {
+    expect(posts(undefined, {})).toEqual(initialState);
+  });
+
+  it('adds posts', () => {
+    const fetchedPosts = [1, 2, 3];
+    const expected = {
+      ...initialState,
+      posts: fetchedPosts,
+    };
+    expect(posts(undefined, { type: GET_POSTS, posts: fetchedPosts })).toEqual(expected);
+  });
+
+  it('updates started fetching posts state', () => {
+    const expected = {
+      ...initialState,
+      startedFetching: true,
+      finishedFetching: false,
+    };
+    expect(posts(undefined, { type: STARTED_FETCHING_POSTS })).toEqual(expected);
+  });
+
+  it('updates finished fetching posts state', () => {
+    const expected = {
+      ...initialState,
+      startedFetching: false,
+      finishedFetching: true,
+    };
+    expect(posts(undefined, { type: FINISHED_FETCHING_POSTS })).toEqual(expected);
+  });
+});
