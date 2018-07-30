354dfce3383271531d99c5aa4112dc8bcebdf6f3
Matt Tuchfarber <matt@tuchfarber.com>
2018-07-30 17:35:26 -0400

    Run libraries though babel
    Libraries were not run through babel and thus the bundles failed the
    es5 check.

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -22,6 +22,7 @@ module.exports = Merge.smart(commonConfig, {
       {
         test: /\.(js|jsx)$/,
         include: [
+          path.resolve(__dirname, '../node_modules'),
           path.resolve(__dirname, '../src'),
         ],
         loader: 'babel-loader',
