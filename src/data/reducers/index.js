b23d34bdec7bab8dc8429aab09029ad137a0a5f8
Adam Stankiewicz <agstanki@gmail.com>
2019-03-08 10:44:51 -0500

    Initial starting point for MFE workshop at Open edX 2019

--- src/data/reducers/index.js
+++ src/data/reducers/index.js
@@ -1,9 +1,6 @@
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 
-import posts from './posts';
-import comment from './comment';
-
 const identityReducer = (state) => {
   const newState = { ...state };
   return newState;
@@ -14,6 +11,4 @@ export default history => combineReducers({
   // The authentication state is added as initialState when
   // creating the store in data/store.js.
   authentication: identityReducer,
-  posts,
-  comment,
 });
