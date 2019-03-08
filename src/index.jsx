e270952a513bad5b1c751196213712d9d6a00630
Adam Stankiewicz <agstanki@gmail.com>
2019-03-08 10:44:51 -0500

    Initial starting point for MFE workshop at Open edX 2019

--- src/index.jsx
+++ src/index.jsx
@@ -4,57 +4,34 @@ import ReactDOM from 'react-dom';
 import { Route, Switch, Link } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
-import { PrivateRoute } from '@edx/frontend-auth';
 
-import PostsPage from './containers/PostsPage';
-import CommentSearchPage from './containers/CommentSearchPage';
-import DisclosurePage from './containers/DisclosurePage';
 import history from './data/history';
 import store from './data/store';
+import apiClient from './data/apiClient';
 
 import './App.scss';
-
-import apiClient from './data/apiClient';
 
 const App = () => (
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <div>
+      <React.Fragment>
         <header>
           <nav>
             <ul className="nav">
               <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/posts">Posts</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/comment-search">Comment Search</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/public/disclosure">Disclosure</Link></li>
             </ul>
           </nav>
         </header>
-        <main>
-          <Switch>
-            <Route path="/public/disclosure" component={DisclosurePage} />
-            <Route path="/public/hello" component={() => <span>Hello World, open route</span>} />
-            <PrivateRoute
-              path="/posts"
-              component={PostsPage}
-              authenticatedAPIClient={apiClient}
-              redirect={`${process.env.BASE_URL}`}
-            />
-            <PrivateRoute
-              path="/comment-search"
-              component={CommentSearchPage}
-              authenticatedAPIClient={apiClient}
-              redirect={`${process.env.BASE_URL}`}
-            />
-            <PrivateRoute
-              path="/"
-              component={() => <span>Hello World, Private route</span>}
-              authenticatedAPIClient={apiClient}
-              redirect={`${process.env.BASE_URL}`}
-            />
-          </Switch>
+        <main className="container mb-4">
+          <div className="row">
+            <div className="col">
+              <Switch>
+                <Route exact path="/" component={() => <p>Hello World!</p>} />
+              </Switch>
+            </div>
+          </div>
         </main>
-      </div>
+      </React.Fragment>
     </ConnectedRouter>
   </Provider>
 );
