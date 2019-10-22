915b1a35953b73178a64b838b310342e7682cfb2
David Joy <djoy@edx.org>
2019-10-22 13:51:01 -0400

    Ensure we create the store for the right environment.

--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-import configureStoreProd from './configureStore.prod';
-import configureStoreDev from './configureStore.dev';
-
-export default function configureStore(state, env) {
-  if (env === 'production') {
-    return configureStoreProd(state);
-  }
-  return configureStoreDev(state);
-}
