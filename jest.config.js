36441922c1b6513dd91e4c5177e7cc28ae66d2fc
David Joy <davidjoy@users.noreply.github.com>
2021-03-30 13:04:12 -0400

    Update jest.config.js (#324)

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 const { createConfig } = require('@edx/frontend-build');
 
 module.exports = createConfig('jest', {
-  setupFiles: [
+  // setupFilesAfterEnv is used after the jest environment has been loaded.  In general this is what you want.  
+  // If you want to add config BEFORE jest loads, use setupFiles instead.  
+  setupFilesAfterEnv: [
     '<rootDir>/src/setupTest.js',
   ],
   coveragePathIgnorePatterns: [
