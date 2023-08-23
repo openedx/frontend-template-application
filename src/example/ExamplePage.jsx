f990ab865d4a0a9b39561a156755b15cd40e440b
Adam Stankiewicz <agstanki@gmail.com>
2023-08-23 10:46:48 -0400

    feat: add start:with-theme and build:with-theme NPM scripts with Paragon CLI

--- src/example/ExamplePage.jsx
+++ src/example/ExamplePage.jsx
@@ -1,10 +1,11 @@
-import { Container } from '@edx/paragon';
+import { Container, Skeleton } from '@edx/paragon';
 
 const ExamplePage = () => (
   <main>
     <Container className="py-5">
       <h1>Example Page</h1>
       <p>Hello world!</p>
+      <Skeleton />
     </Container>
   </main>
 );
