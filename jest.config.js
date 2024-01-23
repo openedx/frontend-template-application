80f274586d98438de19f08b67355f957a53d5f71
Brian Smith <112954497+brian-smith-tcril@users.noreply.github.com>
2024-01-23 16:46:16 -0500

    chore(deps): update paragon and frontend-build to openedx scope (#724)

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-const { createConfig } = require('@edx/frontend-build');
+const { createConfig } = require('@openedx/frontend-build');
 
 module.exports = createConfig('jest', {
   // setupFilesAfterEnv is used after the jest environment has been loaded.  In general this is what you want.  
