31b662cfac06679405f296008f8e3cccec970ca2
Artur Filippovskii <118079918+filippovskii09@users.noreply.github.com>
2025-12-01 15:48:00 +0200

    feat: Transfer frontend-template-application improvements (#944)
    * feat: Transfer frontend-template-application improvements
    
    * fix: fix disscusions and some refactor
    
    * fix: correct header and footer packeges placement
    
    * fix: fix spacing
    
    * fix: ci fix for stylelint
    
    * fix: add correct changes for packages
    
    * fix: fix lint errors
    
    * fix: add correct eslint config
    
    * fix: remove extra package and fix jest config
    
    * fix: update lock file

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,8 @@
 const { createConfig } = require('@openedx/frontend-build');
 
 module.exports = createConfig('jest', {
-  // setupFilesAfterEnv is used after the jest environment has been loaded.  In general this is what you want.  
-  // If you want to add config BEFORE jest loads, use setupFiles instead.  
+  // setupFilesAfterEnv is used after the jest environment has been loaded. In general this is what you want.
+  // If you want to add config BEFORE jest loads, use setupFiles instead.
   setupFilesAfterEnv: [
     '<rootDir>/src/setupTest.js',
   ],
@@ -11,3 +11,5 @@ module.exports = createConfig('jest', {
     'src/i18n',
   ],
 });
+
+export {};
--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,11 @@ import {
   APP_INIT_ERROR, APP_READY, subscribe, initialize,
 } from '@edx/frontend-platform';
 import { AppProvider, ErrorPage } from '@edx/frontend-platform/react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 
 import Header from '@edx/frontend-component-header';
 import { FooterSlot } from '@edx/frontend-component-footer';
@@ -14,19 +18,26 @@ import ExamplePage from './example/ExamplePage';
 
 import './index.scss';
 
+const queryClient = new QueryClient();
+
+const container = document.getElementById('root');
+const root = createRoot(container!);
+
 subscribe(APP_READY, () => {
-  ReactDOM.render(
+  root.render(
     <AppProvider>
-      <Header />
-      <ExamplePage />
-      <FooterSlot />
+      <QueryClientProvider client={queryClient}>
+        <Header />
+        <ExamplePage />
+        <FooterSlot />
+      </QueryClientProvider>
     </AppProvider>,
     document.getElementById('root'),
   );
 });
 
-subscribe(APP_INIT_ERROR, (error) => {
-  ReactDOM.render(<ErrorPage message={error.message} />, document.getElementById('root'));
+subscribe(APP_INIT_ERROR, (error: { message: any }) => {
+  root.render(<ErrorPage message={error.message} />, document.getElementById('root'));
 });
 
 initialize({
